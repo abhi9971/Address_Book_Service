4e52dff370eac0f8f0ee2fd850833401b6ea1eb2
abhi9971 <abhishekrajawat101gmail.com>
2022-02-21 18:53:29 +0530

    Create a new Spring Address Book Application service

---
+++ src/test/java/com/addressbookservice/Address_Book_Service/AddressBookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addressbookservice.Address_Book_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
