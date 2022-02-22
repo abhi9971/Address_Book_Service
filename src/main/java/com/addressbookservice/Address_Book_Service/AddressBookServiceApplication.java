0d75ae5643be916f2c0f7daef09b432396c2da77
abhi9971 <abhishekrajawat101gmail.com>
2022-02-22 16:29:39 +0530

    Use lombk library for logging

--- src/main/java/com/addressbookservice/Address_Book_Service/AddressBookServiceApplication.java
+++ src/main/java/com/addressbookservice/Address_Book_Service/AddressBookServiceApplication.java
@@ -1,14 +1,15 @@
 package com.addressbookservice.Address_Book_Service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookServiceApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to address book ");
 		SpringApplication.run(AddressBookServiceApplication.class, args);
+		log.info("AddressBook service app started..! ");
 	}
-
 }
